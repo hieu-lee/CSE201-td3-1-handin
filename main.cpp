b33e77732bae89d07ffa1ad2e4cfe66b8e663630
hieu-lee <hiuld1009@gmail.com>
2021-09-28 01:32:36 +0200

    Better message

--- main.cpp
+++ main.cpp
@@ -35,7 +35,7 @@ int main(int argc, char* argv[])
   // This code will be executed only if you set GRADING to a value different from 1
 
   {
-	std::cout << "First git commit" << std::endl;
+	std::cout << "A better message" << std::endl;
   }
   // END OF THE CUSTOM CODE SECTION
 #else
