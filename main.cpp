e4845e46970b347dc4b5b8368a00624365965623
hieu-lee <hiuld1009@gmail.com>
2021-09-28 01:27:59 +0200

    Commit message

--- main.cpp
+++ main.cpp
@@ -20,7 +20,7 @@
  * GRADING to 0.
  */
 
-#define GRADING 1
+#define GRADING 0
 
 #include <iostream>
 #include "grading/grading.hpp"
@@ -35,6 +35,7 @@ int main(int argc, char* argv[])
   // This code will be executed only if you set GRADING to a value different from 1
 
   {
+	std::cout << "First git commit" << std::endl;
   }
   // END OF THE CUSTOM CODE SECTION
 #else
